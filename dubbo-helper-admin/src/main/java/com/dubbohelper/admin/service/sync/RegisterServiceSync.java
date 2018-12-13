2ea3dec4aff11074797d5530c04cd667c3a3ea5e
lijinbo <lijinbo@zbj.com>
2018-12-13 13:56:46 +0800

    updte

--- dubbo-helper-admin/src/main/java/com/dubbohelper/admin/service/sync/RegisterServiceSync.java
+++ dubbo-helper-admin/src/main/java/com/dubbohelper/admin/service/sync/RegisterServiceSync.java
@@ -5,7 +5,6 @@ import com.dubbohelper.admin.dto.Application;
 import com.dubbohelper.admin.dto.Constants;
 import com.dubbohelper.admin.dto.URL;
 import com.dubbohelper.admin.dto.Version;
-import com.dubbohelper.admin.service.ConfigureService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
@@ -35,8 +34,6 @@ public class RegisterServiceSync implements InitializingBean, DisposableBean {
 
     @Autowired
     private Config config;
-    @Autowired
-    private ConfigureService configureService;
 
     private static CuratorFramework client;
 
@@ -193,13 +190,13 @@ public class RegisterServiceSync implements InitializingBean, DisposableBean {
      * @throws Exception
      */
     public void conn() throws Exception {
-        if (StringUtils.isEmpty(configureService.getConfigures().getZkAddress())) {
+        if (StringUtils.isEmpty(config.getDubboUrl())) {
             throw new Exception("zk连接地址未设置");
         }
         destroy();
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(10000, 3);
         client = CuratorFrameworkFactory.builder()
-                .connectString(configureService.getConfigures().getZkAddress())
+                .connectString(config.getDubboUrl())
                 .retryPolicy(retryPolicy)
                 .sessionTimeoutMs(10000)
                 .connectionTimeoutMs(10000)
